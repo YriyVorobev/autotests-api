d46ed896ec6582a05cc505ef6fe9d717c31132c9
YriyVorobev <unvhookah@icloud.com>
2025-06-16 16:03:20 +0300

    Практикуемся в реализации API клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,23 +6,24 @@ from typing import TypedDict
 
 
 class CreateUsersRequest(TypedDict):
-        """
-        Описание структуры запроса на создание публичного пользователя
-        """
-        email: str
-        password : str
-        lastName: str
-        firstName: str
-        middleName: str
+    """
+    Описание структуры запроса на создание публичного пользователя
+    """
+    email: str
+    password : str
+    lastName: str
+    firstName: str
+    middleName: str
 
 class PublicUsersClient(APIClient):
     """Клиент для работы /api/v1/users
     """
-    def create_user_api(self,request: CreateUsersRequest )-> Response:
+    def create_user_api(self,
+        request: CreateUsersRequest )-> Response:
         """
         Метод выполняет создание публичного пользователя.
         :param request: Словарь с email, password, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
-        """
+         """
         return self.post("/api/v1/users",json=request)
 
