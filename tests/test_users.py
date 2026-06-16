cbb99789387f370e3f39cbc28e6f294cee82b88d
unvhookah@icloud.com <unvhookah@icloud.com>
2026-06-16 17:51:11 +0300

    Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,13 @@
 from http import HTTPStatus
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema,CreateUserResponseSchema
+from tools.assertions.users import assert_create_user_response
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.users_schema import GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_get_user_response
 import pytest
 
 @pytest.mark.users
@@ -18,3 +22,14 @@ def test_create_user(public_user_client: PublicUsersClient):
     assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data, function_user.response)
+
+    validate_json_schema(response.json(), GetUserResponseSchema.model_json_schema())
